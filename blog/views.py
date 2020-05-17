592ba92fbdd6e8077426809e42a77d658660e5ab
Nicholas Cueto <nicholascueto@gmail.com>
2020-05-17 16:01:04 -0600

    Added Templates and content blocks. Added app to settings.py and added store app for later use. - Video 3

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Nicholas Cueto',
+        'title': 'Blog Post',
+        'content': 'This is a blog post.',
+        'date': 'May 11, 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'This is another blog post.',
+        'date': 'August 23, 2020'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
